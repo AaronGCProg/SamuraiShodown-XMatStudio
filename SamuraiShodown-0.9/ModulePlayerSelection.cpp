6f3adaf494d0785fab46bed1da3be4d7cc12a7e9
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2019-06-09 21:56:35 +0200

    Update 0.9.21
    Changes:
    -Improved Character selection scene

--- SamuraiShodown-0.9/ModulePlayerSelection.cpp
+++ SamuraiShodown-0.9/ModulePlayerSelection.cpp
@@ -29,15 +29,26 @@ ModulePlayerSelection::ModulePlayerSelection()
 	p1p2selectorwhite.w = 40;
 	p1p2selectorwhite.h = 48;
 
-	jubeiword.x = 222;
-	jubeiword.y = 217;
+	jubeiword.x = 0;
+	jubeiword.y = 0;
 	jubeiword.w = 76;
 	jubeiword.h = 33;
 
-	japanword.x = 252;
-	japanword.y = 75;
+	jubeiwordwhite.x = 0;
+	jubeiwordwhite.y = 33;
+	jubeiwordwhite.w = 76;
+	jubeiwordwhite.h = 33;
+
+	japanword.x = 79;
+	japanword.y = 0;
 	japanword.w = 41;
 	japanword.h = 15;
+
+	japanwordwhite.x = 79;
+	japanwordwhite.y = 16;
+	japanwordwhite.w = 41;
+	japanwordwhite.h = 15;
+
 
 
 }
@@ -60,12 +71,13 @@ bool ModulePlayerSelection::Start()
 {
 	graphics = App->textures->Load("Assets/Sprites/PlayerSelectionBackground.png");
 	ui = App->textures->Load("Assets/Sprites/UIspritesheet2.png");
+	ui2 = App->textures->Load("Assets/Sprites/Jubeiselection.png");
 
 	App->audio->soundtracks[2] = Mix_LoadMUS("Assets/Music/Player_Select.ogg");
 	Mix_PlayMusic(App->audio->soundtracks[2], 2);
 
 	App->audio->effects[37] = Mix_LoadWAV("Assets/Music/SelectCharacter.wav");
-	
+
 
 	App->player->Enable();
 	App->player2->Enable();
@@ -80,6 +92,7 @@ bool ModulePlayerSelection::CleanUp()
 	App->audio->CleanUp();
 	App->player->Disable();
 	App->player2->Disable();
+	App->textures->Unload(ui2);
 	App->textures->Unload(ui);
 	App->textures->Unload(graphics);
 
@@ -104,51 +117,76 @@ update_status ModulePlayerSelection::Update()
 {
 	//App->render->Blit(graphics, 0, 0, false, &background, NULL, true);
 
-	
-	if (Timerselect.CountDelayInFrames(60 *30)|| App->input->keyboard[SDL_SCANCODE_SPACE] == 1|| App->input->keyboard[SDL_SCANCODE_P] == 1 || App->input->controller_player1_Start_pressed == true || App->input->controller_player2_Start_pressed == true)
+
+	if (Timerselect.CountDelayInFrames(60 * 30) || App->input->keyboard[SDL_SCANCODE_SPACE] == 1 || App->input->keyboard[SDL_SCANCODE_P] == 1 || App->input->controller_player1_Start_pressed == true || App->input->controller_player2_Start_pressed == true)
 	{
-		if (!selected1 && (App->input->controller_player1_Start_pressed == true|| App->input->keyboard[SDL_SCANCODE_SPACE] == 1))
+		if (!selected1 && (App->input->controller_player1_Start_pressed == true || App->input->keyboard[SDL_SCANCODE_SPACE] == 1))
 		{
 			Mix_PlayChannel(-1, App->audio->effects[37], 0);
 
 			selected1 = true;
+			textwhite1 = true;
+
 		}
-		if (!selected2 && (App->input->controller_player2_Start_pressed == true|| App->input->keyboard[SDL_SCANCODE_P] == 1))
+		if (!selected2 && (App->input->controller_player2_Start_pressed == true || App->input->keyboard[SDL_SCANCODE_P] == 1))
 		{
 			Mix_PlayChannel(-1, App->audio->effects[37], 0);
 
 
 			selected2 = true;
+			textwhite2 = true;
 		}
 	}
 
-	if (!selected1||!selected2) 
+	if (!selected1 || !selected2)
 	{
-		if (Timerend.CountDelayInFrames(60 * 33)) 
-		{ 
-			Mix_FadeOutMusic(2000); 
-			App->fade->FadeToBlack(App->scene_selection, App->scene_hanzo, 2.0f); 
+		if (Timerend.CountDelayInFrames(60 * 33))
+		{
+			Mix_FadeOutMusic(2000);
+			App->fade->FadeToBlack(App->scene_selection, App->scene_hanzo, 2.0f);
 		}
 	}//end
 
-	if (selected1&&selected2) 
-	{ 
-		if (Timerendquick.CountDelayInFrames(60 * 3)) 
-		{ 
+	if (selected1&&selected2)
+	{
+		if (Timerendquick.CountDelayInFrames(60 * 3))
+		{
 			Mix_FadeOutMusic(2000);
-			App->fade->FadeToBlack(App->scene_selection, App->scene_hanzo, 2.0f); 
+			App->fade->FadeToBlack(App->scene_selection, App->scene_hanzo, 2.0f);
 		}
 	}//endquick}
 
 
+		App->render->Blit(ui2, 53, 194, false, &japanword, NULL, true);//japanleft
+		App->render->Blit(ui2, 32, 160, false, &jubeiword, NULL, true);//jubeileft
 
+		App->render->Blit(ui2, 213, 194, false, &japanword, NULL, true);//japanleft
+		App->render->Blit(ui2, 192, 160, false, &jubeiword, NULL, true);//jubeileft
 
+	if (textwhite1)
+	{
+		if (!Timerwhite1.CountDelayInFrames(8))
+		{
+			App->render->Blit(ui2, 53, 194, false, &japanwordwhite, NULL, true);//japanleft
+			App->render->Blit(ui2, 32, 160, false, &jubeiwordwhite, NULL, true);//jubeileft
 
-	App->render->Blit(ui, 53, 194, false, &japanword, NULL, true);//japanleft
-	App->render->Blit(ui, 213, 194, false, &japanword, NULL, true);//japanright
+		}
+		else textwhite1 = false;
+	}
+	
 
-	App->render->Blit(ui, 32, 160, false, &jubeiword, NULL, true);//jubeileft
-	App->render->Blit(ui, 192, 160, false, &jubeiword, NULL, true);//jubeiright
+	if (textwhite2)
+	{
+		if (!Timerwhite1.CountDelayInFrames(8))
+		{
+			App->render->Blit(ui2, 213, 194, false, &japanwordwhite, NULL, true);//japanleft
+			App->render->Blit(ui2, 192, 160, false, &jubeiwordwhite, NULL, true);//jubeileft
+
+		}
+		else textwhite2 = false;
+	}
+	
+
 
 
 	return UPDATE_CONTINUE;
